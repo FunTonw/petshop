95bed228ff161437b11a3bb77da99551e56e0137
FunTonw <qw321888@gmail.com>
2022-02-25 15:47:23 +0800

    add pagination & filters

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -20,10 +20,10 @@
       <td>{{ item.category }}</td>
       <td>{{ item.title }}</td>
       <td class="text-right">
-        {{ item.origin_price }}
+        {{ $filters.currency(item.origin_price) }}
       </td>
       <td class="text-right">
-        {{ item.price }}
+        {{ $filters.currency(item.price) }}
       </td>
       <td>
         <span class="text-success" v-if="item.is_enabled">啟用</span>
@@ -40,6 +40,7 @@
     </tr>
   </tbody>
 </table>
+<PaginationVue :pages="pagination" @emit-pages="getProducts"></PaginationVue>
 <ProductsModal
  ref="productsModal"
  :product="tempProduct"
@@ -53,6 +54,7 @@
 <script>
 import ProductsModal from '../components/ProductsModal.vue';
 import DelModal from '../components/DelModal.vue';
+import PaginationVue from '../components/Pagination.vue';
 
 export default {
   data() {
@@ -67,11 +69,12 @@ export default {
   components: {
     ProductsModal,
     DelModal,
+    PaginationVue,
   },
   inject: ['emitter'],
   methods: {
-    getProducts() {
-      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/products`;
+    getProducts(page = 1) {
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/products/?page=${page}`;
       this.isLoading = true;
       this.$http.get(api)
         .then((res) => {
@@ -79,7 +82,6 @@ export default {
           if (res.data.success) {
             this.products = res.data.products;
             this.pagination = res.data.pagination;
-          }
         });
     },
     openModal(isNew, item) {
