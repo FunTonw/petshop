7d0611bed7e3882438dd18b861d904010323b05a
FunTonw <qw321888@gmail.com>
2022-03-02 21:28:26 +0800

    add: add Coupon

---
+++ src/components/couponModel.vue
@@ -0,0 +1,103 @@
+<template>
+<div>
+  <div class="modal" tabindex="-1" ref="modal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+       <div class="modal-header">
+         <h5 class="modal-title">新增優惠卷</h5>
+         <button type="button"
+          class="btn-close"
+          data-bs-dismiss="modal"
+          aria-label="Close"></button>
+       </div>
+       <div class="modal-body">
+        <div>
+          <div class="mb-3">
+            <label for="title" class="form-label">優惠卷名稱</label>
+            <input type="text" class="form-control" id="title"
+                v-model="couponItem.title"
+                placeholder="請輸入名稱">
+          </div>
+            <div class="mb-3">
+              <label for="price" class="form-label">優惠卷折扣(百分比)</label>
+              <input type="number" class="form-control" id="unit"
+                v-model="couponItem.percent"
+                placeholder="請輸入折扣">
+            </div>
+            <div class="mb-3">
+              <label for="origin_price" class="form-label">使用期限</label>
+              <input type="date" class="form-control" id="origin_price"
+                v-model="changTime"
+                placeholder="請輸入日期">
+            </div>
+            <div class="mb-3">
+              <label for="price" class="form-label">優惠卷代碼</label>
+              <input type="text" class="form-control" id="price"
+                v-model="couponItem.code"
+                placeholder="請輸入代碼名稱">
+            </div>
+
+          <div class="mb-3">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox"
+                      :true-value="1"
+                      :false-value="0"
+                      v-model="couponItem.is_enabled"
+                      id="is_enabled">
+              <label class="form-check-label" for="is_enabled">
+                是否啟用
+              </label>
+            </div>
+          </div>
+        </div>
+       </div>
+       <div class="modal-footer">
+         <button
+         type="button"
+         class="btn btn-primary"
+         data-bs-dismiss="modal"
+         @click="hideModal();"
+         >取消</button>
+         <button
+          type="button"
+          class="btn btn-danger"
+         @click.prevent="getCoupon()">確認</button>
+       </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import modalMixin from '@/mixin/ModalMixin';
+
+export default {
+  data() {
+    return {
+      changTime: '',
+      couponItem: {},
+    };
+  },
+  mixins: [modalMixin],
+  methods: {
+    getCoupon() {
+      this.$emit('emitCoupon', this.couponItem);
+    },
+  },
+  props: {
+    coupon: {
+      type: Object,
+      default() { return {}; },
+    },
+  },
+  watch: {
+    changTime() {
+      this.couponItem.due_date = new Date(this.changTime).getTime();
+    },
+    couponItem() {
+      this.couponItem.due_date = new Date(this.changTime) / 1000;
+    },
+  },
+};
+</script>
--- src/views/Coupon.vue
+++ src/views/Coupon.vue
@@ -1,10 +1,91 @@
 <template>
-<div>
-  <h1>
-    Coupon
-  </h1>
-</div>
+<CouponModel
+ ref="couponModel"
+ @emitCoupon="addCoupon"
+ />
+<button class="btn btn-danger" @click.prevent="openModal()">新增優惠卷</button>
+<table class="table mt-4">
+  <thead>
+    <tr>
+      <th width="120">分類</th>
+      <th>優惠卷名稱</th>
+      <th width="120">折扣百分比</th>
+      <th width="120">折扣代碼</th>
+      <th width="100">是否啟用</th>
+      <th width="200">編輯</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="item in coupons.coupons" :key="item.due_data">
+      <td>{{ cangeTime(item.due_date) }}</td>
+      <td>{{ item.title }}</td>
+      <td class="text-right">
+        {{ item.percent }}
+      </td>
+      <td class="text-right">
+        {{ item.code }}
+      </td>
+      <td>
+        <span class="text-success"
+        v-if="item.is_enabled = '1'">啟用</span>
+        <span class="text-success"
+        v-else>不啟用</span>
+      </td>
+      <td>
+        <div class="btn-group">
+          <button class="btn btn-outline-primary btn-sm">編輯</button>
+          <button class="btn btn-outline-danger btn-sm">刪除</button>
+        </div>
+      </td>
+    </tr>
+  </tbody>
+</table>
 </template>
 
 <script>
+import CouponModel from '../components/couponModel.vue';
+
+export default {
+  data() {
+    return {
+      coupons: {},
+      couponsItem: {},
+    };
+  },
+  methods: {
+    getCoupons() {
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/coupons`;
+      this.$http.get(api)
+        .then((res) => {
+          this.coupons = res.data;
+          console.log(this.coupons);
+        });
+    },
+    openModal() {
+      this.$refs.couponModel.showModal();
+    },
+    addCoupon(item) {
+      this.couponsItem = { ...item };
+      this.$refs.couponModel.hideModal();
+      console.log(this.couponsItem);
+      console.log('sendOK');
+      // api
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/coupon`;
+      // const couponMethods = 'post';
+      this.$http.post(api, { data: this.couponsItem })
+        .then((res) => {
+          console.log(res.data);
+          this.getCoupons();
+        });
+    },
+    cangeTime(time) {
+      const date = new Date(time);
+      return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+    },
+  },
+  created() {
+    this.getCoupons();
+  },
+  components: { CouponModel },
+};
 </script>
