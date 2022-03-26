bc8a4f65ecde0f6131d6a6905e8efb17c1c73321
FunTonw <qw321888@gmail.com>
2022-03-26 15:31:02 +0800

    add: CustomerSaid

--- src/components/CustomersSaid.vue
+++ src/components/CustomersSaid.vue
@@ -1,41 +1,36 @@
 <template>
-  <div class="container-0">
-<div class="swiper" id="customer">
+  <div class="container-0 py-5" style="background-color:#666699;">
+   <h3 class="text-center text-white fsadd">顧客回饋</h3>
+   <div class="container">
+     <div class="swiper" id="customer">
+      <div class="swiper-pagination"></div>
       <div class="swiper-wrapper">
-        <div class="swiper-slide">
-          <div class="customer-bg">
-            <p>ADAM</p>
-            <p>CUSTOMER</p>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.
-            </p>
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <div>
-            <p>BDAM</p>
-            <p>CUSTOMER</p>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.
-            </p>
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <div>
-            <p>ADANN</p>
-            <p>CUSTOMER</p>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.
-            </p>
+        <div class="swiper-slide" v-for="item in Customers" :key="item.id">
+          <div class="customer-card d-flex flex-column align-items-center py-3 text-white">
+            <div class="mb-4">
+              <img :src="item.pic" :alt="item.name">
+            </div>
+            <div class="lh-1">
+              <p class="fs-5">{{ item.name }}</p>
+              <p class="fs-6">{{ item.appellation }}</p>
+              <p class="fs-6 lh-lg">
+                {{ item.say }}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-</div>
+     </div>
+   </div>
   </div>
 </template>
 <style>
-  .customer-bg{
+  .customer-card{
     background: #666699;
+  }
+  .customer-card > div >img {
+    border-radius: 50%;
+    max-width: 100%;
   }
 </style>
 
@@ -44,10 +39,54 @@ import Swiper, { Grid, Navigation, Pagination } from 'swiper';
 import 'swiper/swiper-bundle.min.css';
 import 'swiper/swiper.min.css';
 
+import Adamimg from '../assets/img/customer/1-150x150.jpg';
+import Lisaimg from '../assets/img/customer/2-150x150.jpg';
+import Judyimg from '../assets/img/customer/3-150x150.jpg';
+
 export default {
   data() {
     return {
-      Customers: [],
+      Customers: [
+        {
+          id: '5195192',
+          pic: Adamimg,
+          name: 'ADAM',
+          appellation: 'CUSTOMER',
+          say:
+            `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.`,
+        },
+        {
+          id: '53453t3',
+          pic: Lisaimg,
+          name: 'LISA',
+          appellation: 'CUSTOMER',
+          say:
+            `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.`,
+        },
+        {
+          id: '56s8d65',
+          pic: Judyimg,
+          name: 'Judy',
+          appellation: 'CUSTOMER',
+          say:
+            `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, tenetur.`,
+        },
+      ],
     };
   },
   mounted() {
@@ -55,6 +94,10 @@ export default {
       modules: [Grid, Navigation, Pagination],
       slidesPerView: 1,
       spaceBetween: 20,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
     });
   },
 };
