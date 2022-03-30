1a8dbbb297fc744c7a67c0e124732ebd7e5168ad
FunTonw <qw321888@gmail.com>
2022-03-30 20:42:29 +0800

    edit: Navbar animation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="header">
     <div class="container">
-      <div class="d-flex justify-content-between p-2 ">
-        <ul class="d-flex m-0 content-list">
-          <li>
+      <div class="d-flex justify-content-between align-items-center p-2 ">
+        <ul class="d-flex
+         flex-column flex-lg-row
+         m-0
+         content-list">
+          <li class="me-3">
             <a href="tel:+078005544"><i class="bi bi-telephone-fill"></i> 07-8005544</a>
           </li>
-          <li class="ms-3">
+          <li>
             <a href="mailto:heHa@yahoo.com"><i class="bi bi-envelope"></i> CakeKing@CK.com</a>
           </li>
         </ul>
--- src/components/UserNavbar.vue
+++ src/components/UserNavbar.vue
@@ -1,27 +1,43 @@
 <template>
-  <nav class="navbar navbar-expand-lg nav-text shadow-sm p-4">
+  <nav class="
+  nav-text
+  shadow-sm
+  p-md-4 p-0
+  bg-light">
     <div class="container-fluid">
       <div class="d-flex align-items-center justify-content-around w-100">
         <a href="#" class="navbar-brand"><img src="../assets/img/logo.png" alt=""></a>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mx-auto mb-2 mb-lg-0 nav-list under-line">
-            <li class="nav-item">
-              <a class="me-4" aria-current="page" href="#">Home</a>
+          <ul class="
+          d-flex
+          flex-md-row flex-column
+          mx-auto
+          p-0
+          nav-list
+          user-nav-list
+          bg-light
+          under-line"
+          ref="navList">
+            <li class="px-auto">
+              <a class="py-1 me-lg-4 me-0 text-center d-block" aria-current="page" href="#">Home</a>
             </li>
-            <li class="nav-item me-4">
+            <li class="py-1 me-lg-4 me-0 text-center d-block">
               <a href="#">About Us</a>
             </li>
-            <li class="nav-item me-4">
+            <li class="py-1 me-lg-4 me-0 text-center d-block">
               <a href="#">Product</a>
             </li>
-            <li class="nav-item me-4">
+            <li class="py-1 me-lg-4 me-0 text-center d-block ">
               <a href="#">Shop</a>
             </li>
           </ul>
-        </div>
-        <ul class="d-flex nav-list">
+        <ul class="d-flex nav-list m-0">
           <li><a href="#"><i class="bi bi-cart4 fs-4 me-3"></i></a></li>
-          <li><a href="#"><i class="bi bi-person-circle fs-4"></i></a></li>
+          <li><a href="#"><i class="bi bi-person-circle fs-4 me-3"></i></a></li>
+          <li>
+            <a href="#" class="nav-ham" @click.prevent="listActive()">
+              <i class="bi bi-list fs-4"></i>
+            </a>
+          </li>
         </ul>
       </div>
     </div>
@@ -48,4 +64,55 @@
   .under-line > li > a:hover::before{
     width: 100%;
   }
+  .nav-ham{
+    display: none;
+  }
+  @media screen and (max-width: 429px) {
+    .nav-ham{
+      display: block;
+    }
+    .nav{
+      position: relative;
+    }
+    .user-nav-list{
+      position: absolute;
+      width: 100%;
+      background: #fff;
+      transform: translate(100%, 80%);
+      display: block;
+      opacity: 1;
+      z-index: 1;
+      transition: all .2s linear;
+    }
+    /* .user-nav-list > li{
+      height: 0;
+    } */
+    .list-active {
+      /* transition: all .5 linear;
+      height: 100% !important */
+      transition: all .2s linear;
+      transform: translate(0%, 80%);
+      opacity: 1;
+    }
+   }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      listActiveSwitch: true,
+    };
+  },
+  methods: {
+    listActive() {
+      this.listActiveSwitch = !this.listActiveSwitch;
+      if (!this.listActiveSwitch) {
+        this.$refs.navList.classList.add('list-active');
+      } else {
+        this.$refs.navList.classList.remove('list-active');
+      }
+    },
+  },
+};
+</script>
